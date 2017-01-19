152a9e3fcc0fa1ae497814d4b06d2fd69c7c372a
akhil451 <akhilprasannan451@gmail.com>
2017-01-19 22:54:15 +0530

    added index.html,mystyles.css and myscrippts.js

---
+++ myscripts.js
@@ -0,0 +1,106 @@
+      $(document).ready(function(){
+  var countpom=parseInt($("#pomtime").html());
+   var countbreak=parseInt($("#breaktime").html());
+  console.log(countpom);
+  console.log(countbreak);
+   windowHeight = $(window).innerHeight();
+    $('#pomodoro').css('min-height', windowHeight*0.5);
+      $('#break').css('min-height', windowHeight*0.5);
+
+  $("#reducepomodoro").click(function(){
+  if(countpom>0){
+    countpom--;
+    $("#pomtime").html(countpom);
+  }
+});
+
+   $("#addpomodoro").click(function(){
+  if(countpom<45){
+    countpom++;
+    $("#pomtime").html(countpom);
+  }
+});
+
+  $("#reducebreak").click(function(){
+  if(countbreak>0){
+    countbreak--;
+    $("#breaktime").html(countbreak);
+  }
+});
+  $("#addbreak").click(function(){
+  if(countbreak<20){
+    countbreak++;
+    $("#breaktime").html(countbreak);
+  }
+});
+
+
+  var cp=countpom*60;
+    var cb=countbreak*60;
+
+  $("#start").click(function(){
+    countpom=cp;
+    countbreak=cb;
+    $("#pomodoro,#start").css("visibility","visible");
+       $("#break,.timepomo").css('visibility','hidden');
+       var pomocounter=setInterval(pomcallback, 1000);
+        function pomcallback(){
+
+             countpom--;
+         if(countpom===0)
+          { $("#break").css('visibility','visible');
+           $("#break").css('visibilty','visible');
+
+            clearInterval(pomocounter);
+             countpom=cp;
+             var breakcounter=setInterval(breakcallback,1000);
+            function breakcallback(){
+             $("#timebreak").css('visibility','hidden')
+            //  $("#breakclock").html(countbreak);
+
+               $("#pomodoro").css('visibility','hidden');
+ if(countbreak%60>10)
+          {
+
+            $("#breakclock").html(Math.floor(countbreak/60)+":"+countbreak%60);
+
+          }
+          else{
+
+            $("#breakclock").html(Math.floor(countbreak/60)+": 0"+countbreak%60);
+          }
+              countbreak--;
+              if(countbreak===0){
+clearInterval(breakcounter);
+countbreak=cb;
+            $("#start").click();
+              }
+
+            }
+          }
+          if(countpom%60>10)
+          {
+
+            $("#pomodoroclock").html(Math.floor(countpom/60)+":"+countpom%60);
+
+          }
+          else{
+
+            $("#pomodoroclock").html(Math.floor(countpom/60)+": 0"+countpom%60);
+          }
+          //$("#pomodoroclock").html(countpom);
+
+        }
+  });// end of start
+
+$("#reset").click(function(){
+  location.reload();
+  //page refresh;
+});
+
+
+
+});// end of document.ready();
+
+
+
